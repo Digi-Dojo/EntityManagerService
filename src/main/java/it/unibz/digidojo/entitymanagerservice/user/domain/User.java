e5c6da5ceb293f37facb4603417107c0aa6509ed
Thiago Guerrero <tgbalera@amazon.com>
2023-11-12 18:51:34 -0300

    Resolve usage of reserved words without a persistence.xml file
    Signed-off-by: Thiago Guerrero <tgbalera@amazon.com>

--- src/main/java/it/unibz/digidojo/entitymanagerservice/user/domain/User.java
+++ src/main/java/it/unibz/digidojo/entitymanagerservice/user/domain/User.java
@@ -9,9 +9,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "user_")
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
